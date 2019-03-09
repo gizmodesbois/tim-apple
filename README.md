cabe31fea90ab3d9d0d5908b384ffd4b85ad4615
Simon Henrotte <henrotte.simon@gmail.com>
2019-03-09 13:48:37 +0100

    Change to only apple.com

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Tim Apple
 
-Will replace Tim Cook by Tim Apple in Chrome except on this specific page.
+Will replace Tim Cook by Tim Apple in Chrome on Apple's website.
--- src/custom.js
+++ src/custom.js
@@ -1,10 +1,5 @@
 var all = document.querySelectorAll("h1,h2,h3,h4,h5,p,span,em,strong");
 
-var searchValue = ['Tim Cook', 'tim cook'];
-for (j = 0; j < searchValue.length; j++) {
-  for (i = 0; i < all.length; i++) {
-    if (all[i].innerHTML.indexOf(searchValue[j]) > -1) {
-      all[i].innerHTML = all[i].innerHTML.replace(searchValue[j], "Tim Apple")
-    }
-  }
+for (i = 0; i < all.length; i++) {
+  all[i].innerHTML = all[i].innerHTML.replace(/tim cook/gi, "Tim Apple")
 }
